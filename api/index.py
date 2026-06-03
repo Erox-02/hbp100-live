2527abe8f3f3a0308b1690ba7c48d89c2e5edc52
Tech_.erox <relic.of.sky@gmail.com>
2026-06-03 16:40:45 +0530

    Ahh i added the backends url in backends cors wow

--- api/index.py
+++ api/index.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://hbp100-live-api.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://hbp-100.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
